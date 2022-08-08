6f52dcd8c9d64f1337a5e820217b63c0d06a9ac0
sindre-haugen <sindre.haugen@ssb.no>
2022-08-08 10:24:02 +0200

    Update description

--- document_package.R
+++ document_package.R
@@ -5,6 +5,9 @@ devtools::document()
 # Add imports
 usethis::use_package("dplyr", type = "imports")
 
+usethis::use_package("RODBC", type = "suggests") # OBS: kun installert i RStudio Windows
+usethis::use_package("ROracle", type = "suggests") # OBS: ikke installert i RStudio Windows
+
 # Vignette #
 devtools::load_all()
 usethis::use_pkgdown() # OBS: docs legges i .gitignore (må fjernes)
