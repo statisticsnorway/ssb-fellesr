5b0086bb98795c0b65f97500d36f381d8b8a78f1
sindre-haugen <sindre.haugen@ssb.no>
2022-08-08 15:24:09 +0200

    Add usethis::use_pipe()

--- document_package.R
+++ document_package.R
@@ -8,6 +8,8 @@ usethis::use_package("dplyr", type = "imports")
 usethis::use_package("RODBC", type = "suggests") # OBS: kun installert i RStudio Windows
 usethis::use_package("ROracle", type = "suggests") # OBS: ikke installert i RStudio Windows
 
+usethis::use_pipe()
+
 # Vignette #
 devtools::load_all()
 usethis::use_pkgdown() # OBS: docs legges i .gitignore (må fjernes)
