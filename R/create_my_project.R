ef2a41a457a1031b0776e784adf738d100a91e2a
Kari <coo@ssb.no>
2024-01-23 11:29:32 +0100

    Removed old template function

--- R/create_my_project.R
+++
@@ -1,8 +0,0 @@
-#' Create a new project from my minimal template
-#'
-#' @param path The path to create the new project
-#' @export
-create_my_project <- function(path) {
-  message("Creating project at ", path)
-  # Here you'd normally set up the project directory, etc.
-}
