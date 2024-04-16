79c24a6ddcca6a0dd1db4421d5835e8c99b596c2
ssb-yib <yib@ssb.no>
2024-04-16 13:18:31 +0000

    Omskriving av funksjoner

--- R/import_eksport.R
+++ R/import_eksport.R
@@ -115,37 +115,89 @@ import_gcs <- function(file, ...) {
 
 
 
-write_ssb <- function(data, file) {
+#' Eksport av filer på Dapla og på bakken
+#'
+#' @inheritParams export_gcs
+#'
+#' @return
+#'
+#' På bakken: resultatet av eksporten som returnert av [rio::export].
+#'
+#' På Dapla: resultatet av eksporten som returnert av [googleCloudStorageR::gcs_upload]
+#'
+#' @details
+#'
+#' Se [rio::export] for tilgjengelige filformater. Funksjonen støtter de samme paramete
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' write_ssb(mtcars, "ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_xlsx_test.xlsx")
+#'
+#' write_ssb(mtcars, "ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_csv_test.csv")
+#'
+#' write_ssb(mtcars, "ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_parquet_test.parquet")
+#'
+#' write_ssb(mtcars, "ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_rds_test.rds")
+#'
+#' write_ssb(data = list(foo = "bar", egg = "ham"),
+#'           file = "ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_json_test.json")
+#'
+#' }
+#'
+write_ssb <- function(data, file, ...) {
 
-  if (Sys.getenv("DAPLA_REGION") %in% c("BIP", "DAPLA_LAB")) {
+  write_fun <- switch(Sys.getenv("DAPLA_REGION"),
+                      BIP       = export_gcs,
+                      DAPLA_LAB = export_gcs,
+                      ON_PREM   = rio::export,
+                      stop("Ukjent miljø, avbryter"))
 
-
-
-  } else if (Sys.getenv("DAPLA_REGION") == "ON_PREM") {
-
-
-  } else {
-
-    stop("Ukjent miljø, avbryter")
-
-  }
+  return(write_fun(data, file, ...))
 
 }
 
+#' @rdname write_ssb
+#' @export
+write_SSB <- write_ssb
+
+
+#' Import av filer på Dapla og på bakken
+#'
+#' @inheritParams import_gcs
+#' @inheritParams rio::import
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' \dontrun{
+#'
+#' read_ssb("ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_xlsx_test.xlsx")
+#' read_ssb("ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_csv_test.csv")
+#' read_ssb("ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_parquet_test.parquet")
+#' read_ssb("ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_rds_test.rds")
+#' read_ssb("ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_json_test.json")
+#'
+#'
+#' }
+#'
 read_ssb <- function(file, ...) {
 
-  if (Sys.getenv("DAPLA_REGION") %in% c("BIP", "DAPLA_LAB")) {
+  read_fun <- switch(Sys.getenv("DAPLA_REGION"),
+                     BIP       = import_gcs,
+                     DAPLA_LAB = import_gcs,
+                     ON_PREM   = rio::import,
+                     stop("Ukjent miljø, avbryter"))
 
-    return(import_gcs(file, ...))
-
-  } else if (Sys.getenv("DAPLA_REGION") == "ON_PREM") {
-
-    return(rio::import(file, ...))
-
-  } else {
-
-    stop("Ukjent miljø, avbryter")
-
-  }
+  return(read_fun(file, ...))
 
 }
+
+#' @rdname read_ssb
+#' @export
+read_SSB <- read_ssb
+
