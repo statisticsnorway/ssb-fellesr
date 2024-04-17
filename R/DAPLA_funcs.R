9e1fcb4f2c780c02378d3f25f528bea4c59577ed
ssb-yib <yib@ssb.no>
2024-04-17 07:40:17 +0000

    Flytt gcs_export/import til dapla-funcs

--- R/DAPLA_funcs.R
+++ R/DAPLA_funcs.R
@@ -752,89 +752,64 @@ gcs_import <- function(file, ...) {
 }
 
 
-
-#' Eksport av filer på Dapla og på bakken
+#' Eksport av filer til GCS-bøtter med `rio::export`.
 #'
-#' @inheritParams gcs_export
+#' Skriv data.frame og andre objekter til bøtter med  [rio::export].
 #'
-#' @return
+#' @param data objekt som skal eksporteres.
+#' @param file Full sti og filnavn på filen som skal opprettes. Eventuelt
+#'   `gs://`-prefiks fjernes, og det øverste mappenivået tolkes som navn på
+#'   GCS-bøtten.
+#' @param ... Argumenter som sendes videre til [rio::export], og dermed til
+#'   underliggende eksporterende funksjoner.
+#' @param export_function En funksjon med parametere `input` og `output` som
+#'   eksporterer den ønskede filen.
 #'
-#' På bakken: resultatet av eksporten som returnert av [rio::export].
+#' @return Resultatet av eksporten som returnert av [rio::export].
 #'
-#' På Dapla: resultatet av eksporten som returnert av [googleCloudStorageR::gcs_upload]
+#' @details Parameterene `x`, `file`, og alle parametere i `...` blir sendt
+#' videre til [rio::export] (default), eller en funksjon angitt .
 #'
-#' @details
-#'
-#' Se [rio::export] for tilgjengelige filformater. Funksjonen støtter de samme paramete
+#' Se [rio::export] for en oversikt over filformatene som kan eksporteres med
+#' denne funksjonen.
 #'
 #' @export
+#'
+#' @seealso [gcs_import()] og [rio::export()]
 #'
 #' @examples
 #' \dontrun{
 #'
-#' write_ssb(mtcars, "ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_xlsx_test.xlsx")
+#' gcs_export(mtcars, "ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_xlsx_test.xlsx")
 #'
-#' write_ssb(mtcars, "ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_csv_test.csv")
+#' gcs_export(mtcars, "ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_csv_test.csv")
 #'
-#' write_ssb(mtcars, "ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_parquet_test.parquet")
+#' gcs_export(mtcars, "ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_parquet_test.parquet")
 #'
-#' write_ssb(mtcars, "ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_rds_test.rds")
+#' gcs_export(mtcars, "ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_rds_test.rds")
 #'
-#' write_ssb(data = list(foo = "bar", egg = "ham"),
-#'           file = "ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_json_test.json")
-#'
-#' }
-#'
-write_ssb <- function(data, file, ...) {
-
-  write_fun <- switch(Sys.getenv("DAPLA_REGION"),
-                      BIP       = gcs_export,
-                      DAPLA_LAB = gcs_export,
-                      ON_PREM   = rio::export,
-                      stop("Ukjent miljø, avbryter"))
-
-  return(write_fun(data, file, ...))
-
-}
-
-#' @rdname write_ssb
-#' @export
-write_SSB <- write_ssb
-
-
-#' Import av filer på Dapla og på bakken
-#'
-#' @inheritParams gcs_import
-#' @inheritParams rio::import
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#'
-#' \dontrun{
-#'
-#' read_ssb("ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_xlsx_test.xlsx")
-#' read_ssb("ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_csv_test.csv")
-#' read_ssb("ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_parquet_test.parquet")
-#' read_ssb("ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_rds_test.rds")
-#' read_ssb("ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_json_test.json")
-#'
+#' gcs_export(data = list(foo = "bar", egg = "ham"),
+#'            file = "ssb-prod-dapla-felles-data-delt/R_smoke_test/write_SSB_json_test.json")
 #'
 #' }
 #'
-read_ssb <- function(file, ...) {
+gcs_export <- function(data, file, ...) {
 
-  read_fun <- switch(Sys.getenv("DAPLA_REGION"),
-                     BIP       = gcs_import,
-                     DAPLA_LAB = gcs_import,
-                     ON_PREM   = rio::import,
-                     stop("Ukjent miljø, avbryter"))
+  path <- gcs_validate_file_path(file)
 
-  return(read_fun(file, ...))
+  export_function <- function(input, output) {
+
+    do.call(rio::export, c(list(x = input, file = output), list(...)))
+
+  }
+
+  gcs_global_bucket(path["bucket"])
+
+  invisible(
+    googleCloudStorageR::gcs_upload(file = data,
+                                    name = path["file"],
+                                    predefinedAcl = "bucketLevel",
+                                    object_function = export_function)
+  )
 
 }
-
-#' @rdname read_ssb
-#' @export
-read_SSB <- read_ssb
