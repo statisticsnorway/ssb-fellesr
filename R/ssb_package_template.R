c13825d2d8add1407c3b76362deeb7154402ea25
Su <coo@ssb.no>
2024-04-29 13:54:16 +0200

    Added branch protections and fix to avoid double creation of projects.

--- R/ssb_package_template.R
+++ R/ssb_package_template.R
@@ -8,9 +8,9 @@
 #'
 #' @return NULL
 fix_file <- function(destination, file, find, replace){
-  path <- file.path(destination, file)
+  destination_path <- file.path(destination, file)
 
-  content <- readLines(path)
+  content <- readLines(destination_path)
   content <- gsub(find, replace, content, fixed = TRUE)
 
   # Check if the last line is a newline; if not, add one
@@ -18,7 +18,7 @@ fix_file <- function(destination, file, find, replace){
     content <- c(content, "")  # Add an empty string as the new final line
   }
 
-  writeLines(content, path)
+  writeLines(content, destination_path)
 }
 
 
@@ -36,20 +36,23 @@ fix_file <- function(destination, file, find, replace){
 #' @export
 ssb_rtemplate <- function(path, description,
                           firstname, surname, github){
-
+  wd_dir <- getwd()
   base_dir <- dirname(path)
   package_name <- basename(path)
+  original_path <- NULL
 
-  if (substr(package_name, 1,4) == "ssb-"){
+  if (substr(package_name, 1, 4) == "ssb-"){
     prefixed_name <- package_name
-    package_name <- substring(package_name, 5, nchar(pacakge_name))
+    package_name <- substring(package_name, 5, nchar(package_name))
   } else {
+    original_path <- path
     prefixed_name <- paste0("ssb-", package_name)
+    path <- file.path(base_dir, prefixed_name)
   }
 
   # Create the local directory with the prefixed name
-  destination <- file.path(base_dir, prefixed_name)
-  dir.create(destination, recursive = TRUE)
+  dir.create(path, recursive = TRUE)
+  message("Project created at: ", path)
 
   # Specify other variables
   year <- substring(Sys.Date(), 1, 4)
@@ -63,35 +66,35 @@ ssb_rtemplate <- function(path, description,
 
   # Copy each file and directory in template_contents to destination
   for (file in template_contents) {
-    file.copy(file, destination, recursive = TRUE)
+    file.copy(file, path, recursive = TRUE)
   }
   Sys.sleep(2)
 
   # Fix Readme file
-  fix_file(destination, "README.md", find = "{{PACKAGE_NAME}}", package_name)
-  fix_file(destination, "README.md", find = "{{PACKAGE_NAME_CODE}}", prefixed_name)
-  fix_file(destination, "README.md", find = "{{PACKAGE_DESCRIPTION}}", description)
+  fix_file(path, "README.md", find = "{{PACKAGE_NAME}}", package_name)
+  fix_file(path, "README.md", find = "{{PACKAGE_NAME_CODE}}", prefixed_name)
+  fix_file(path, "README.md", find = "{{PACKAGE_DESCRIPTION}}", description)
 
   # Fix description
-  fix_file(destination, "DESCRIPTION", find = "{{PACKAGE_NAME}}", package_name)
-  fix_file(destination, "DESCRIPTION", find = "{{PACKAGE_DESCRIPTION}}", description)
-  fix_file(destination, "DESCRIPTION", find = "{{AUTHOR_NAME1}}", firstname)
-  fix_file(destination, "DESCRIPTION", find = "{{AUTHOR_NAME2}}", surname)
-  fix_file(destination, "DESCRIPTION", find = "{{AUTHOR_EMAIL}}", email)
+  fix_file(path, "DESCRIPTION", find = "{{PACKAGE_NAME}}", package_name)
+  fix_file(path, "DESCRIPTION", find = "{{PACKAGE_DESCRIPTION}}", description)
+  fix_file(path, "DESCRIPTION", find = "{{AUTHOR_NAME1}}", firstname)
+  fix_file(path, "DESCRIPTION", find = "{{AUTHOR_NAME2}}", surname)
+  fix_file(path, "DESCRIPTION", find = "{{AUTHOR_EMAIL}}", email)
 
   # Fix Licence files
-  fix_file(destination, "LICENSE.md", find = "{{YEAR}}", year)
-  fix_file(destination, "LICENSE", find = "{{YEAR}}", year)
+  fix_file(path, "LICENSE.md", find = "{{YEAR}}", year)
+  fix_file(path, "LICENSE", find = "{{YEAR}}", year)
 
   # Fix NEWS
-  fix_file(destination, "NEWS.md", find = "{{PACKAGE_NAME}}", package_name)
+  fix_file(path, "NEWS.md", find = "{{PACKAGE_NAME}}", package_name)
 
   # Fix SECURITY
-  fix_file(destination, "SECURITY.md", find = "{{PACKAGE_NAME}}", package_name)
+  fix_file(path, "SECURITY.md", find = "{{PACKAGE_NAME}}", package_name)
 
   # Fix name of project file
-  setwd(destination)
-  file.rename( "packagename.rproj", paste0(package_name, ".rproj"))
+  setwd(path)
+  file.rename( "packagename.Rproj", paste0(prefixed_name, ".Rproj"))
 
   # Add comments file
   usethis::use_cran_comments(open=F)
@@ -120,8 +123,6 @@ ssb_rtemplate <- function(path, description,
       Sys.setenv(GITHUB_PAT = getPass::getPass("Enter your github PAT (with workflow priveldges):"))
     }
 
-    #gitcreds::gitcreds_set()
-    #print("credentials set?")
     usethis::use_github(organisation = "statisticsnorway",
                         visibility = "internal", protocol = "https")
 
@@ -145,8 +146,34 @@ ssb_rtemplate <- function(path, description,
                             httr::authenticate("", Sys.getenv("GITHUB_PAT")),
                             encode = "json")
     httr::content(response)
+
+    # Add branch protection
+    response <- gh::gh(
+      "PUT /repos/:owner/:repo/branches/:branch/protection",
+      owner = "statisticsnorway",
+      repo = prefixed_name,
+      branch = "main",
+      .token = Sys.getenv("GITHUB_PAT"),
+      required_status_checks = NA,  # No specific status checks are mentioned
+      enforce_admins = TRUE,  # Do not allow bypassing of the settings
+      required_pull_request_reviews = list(
+        dismiss_stale_reviews = TRUE,  # Dismiss stale pull request approvals when new commits are pushed
+        require_code_owner_reviews = FALSE,  # No specific requirement for code owner reviews was mentioned
+        required_approving_review_count = 1  # Require at least one approval
+      ),
+      restrictions = NA,  # No specific user or team restrictions mentioned
+      required_pull_requests_reviews_enforcement_level = "everyone"  # Enforce rules on everyone, including admins
+    )
   }
 
+  # Open new project
+  Sys.sleep(2)
+  setwd(wd_dir)
+  rstudioapi::openProject(path = path, newSession = FALSE)
+
+  # Cancel further processes - this supresses Rstudio from creating a duplicate project (without suffix)
+  # This is currently sending an error message which I haven't been able to suppress.
+  stop("Finished setting up package! This looks like an error but isn't (Just click ok).")
 }
 
 
